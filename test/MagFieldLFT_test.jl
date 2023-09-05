455d5bac30ad7b1c46719850bf4bf2df95b05e1f
Lucas Lang <lucas.lang@campus.tu-berlin.de>
2023-09-05 11:43:51 +0200

    - Implemented unitary transformation matrices from complex to real orbitals. - Hard-coded ck matrices (for expanding 2-ERIs in terms of   Slater--Condon parameters).

--- test/MagFieldLFT_test.jl
+++ test/MagFieldLFT_test.jl
@@ -4,22 +4,31 @@ using MagFieldLFT
 
 function test_iscanonical1()
     orbstring = [1,2,5,5,6]    # same orbital occurs twice: not canonical
-    return iscanonical(orbstring) == false
+    return MagFieldLFT.iscanonical(orbstring) == false
 end
 
 function test_iscanonical2()
     orbstring = [1,2,5,6]  
-    return iscanonical(orbstring) == true
+    return MagFieldLFT.iscanonical(orbstring) == true
 end
 
 function test_iscanonical3()
     orbstring = [2,1,5,6]  
-    return iscanonical(orbstring) == false
+    return MagFieldLFT.iscanonical(orbstring) == false
 end
 
 function test_createSDs()
     ref = [[1,2,3], [1,2,4], [1,3,4], [2,3,4]]
-    create_SDs(3,2) == ref
+    MagFieldLFT.create_SDs(3,2) == ref
+end
+
+function test_U_complex2real()
+    ref = [1/sqrt(2) 0 0 0 -im/sqrt(2);
+           0 -1/sqrt(2) 0 im/sqrt(2) 0;
+           0 0 1 0 0;
+           0 1/sqrt(2) 0 im/sqrt(2) 0;
+           1/sqrt(2) 0 0 0 im/sqrt(2)]
+    return MagFieldLFT.U_complex2real(2) ≈ ref
 end
 
 @testset "MagFieldLFT.jl" begin
@@ -27,4 +36,5 @@ end
     @test test_iscanonical2()
     @test test_iscanonical3()
     @test test_createSDs()
+    @test test_U_complex2real()
 end
